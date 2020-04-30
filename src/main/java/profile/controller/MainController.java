eaa03edff827241ecf152488f879c98a19f7e86e
Alena <alena.chuvasova@gmail.com>
2020-05-01 01:07:50 +0300

    added login users with role permissions and db connection

--- src/main/java/profile/controller/MainController.java
+++ src/main/java/profile/controller/MainController.java
@@ -13,11 +13,11 @@ public class MainController {
 
     @GetMapping("/user")
     public String user() {
-        return ("<h1>Welcome User</h1>");
+        return ("user");
     }
 
     @GetMapping("/admin")
     public String admin() {
-        return ("<h1>Welcome Admin</h1>");
+        return ("admin");
     }
 }
