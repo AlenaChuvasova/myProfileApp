97db99fd2ea19de992aa77f5b1e31a96cce74ae2
Alena <alena.chuvasova@gmail.com>
2020-05-06 21:48:30 +0300

    added main menu with vuejs

--- src/main/java/profile/controller/MainController.java
+++ src/main/java/profile/controller/MainController.java
@@ -3,9 +3,11 @@ package profile.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import profile.entity.User;
 import profile.repository.UserRepository;
 
